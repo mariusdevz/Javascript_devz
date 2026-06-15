61e8f3b33d8939cbca55e24e267dab96908ee736
MariuswebDev <efuetlanchamarius@gmail.com>
2026-06-16 00:16:07 +0100

    Day-2: password validator

---
+++ Day-2/General.md
@@ -0,0 +1,11 @@
+## Week 3: Arrays
+
+### Core Concepts
+
+1. Creating arrays
+2. Accessing array elements
+3. Array methods: push, pop, shift, unshift
+4. Array methods: slice, splice
+5. Array methods: indexOf, includes
+6. Iterating arrays: forEach, map, filter
+7. Higher-order functions
---
+++ Day-2/practice01.js
@@ -0,0 +1,70 @@
+// CREATING ARRAYS
+const fruits = ["apple", "banana", "orange"];
+// const numbers = [1, 2, 3, 4, 5];
+const mixed = [1, "hello", true, null];
+const nested = [
+    [1, 2],
+    [3, 4],
+    [5, 6],
+];
+
+// ACCESSING ELEMENTS (0-indexed)
+console.log(fruits[0]);
+console.log(fruits[2]);
+console.log(fruits.length);
+console.log(fruits.length - 1); //last element
+
+//MUTATING ELEMENTS (change the original array)
+fruits.push("grape"); //add to the end
+console.log(fruits);
+
+fruits.pop; //remove from end
+console.log(fruits);
+
+fruits.unshift("kiwi"); //add to start
+console.log(fruits);
+
+fruits.shift(); // remove from start
+console.log(fruits);
+
+// NON-MUTATING METHODS (don't change original)
+const slice = fruits.slice(1) // ["banana", "orange"]
+console.log(fruits);
+
+//SEARCHING
+console.log(fruits.indexOf("banana")); // 1
+console.log(fruits.indexOf("grape")); // -1 (not found)
+console.log(fruits.includes("apple")); // true
+console.log(fruits.includes("grape")); // false
+
+// FOREACH - just loop through (no return)
+fruits.forEach(function (fruit) {
+    console.log(fruit);
+
+});
+
+// Modern synthax with arrow function
+fruits.forEach((fruit) => console.log(fruit));
+
+// MAP - transform each element (returns new array)
+const numbers = [1, 2, 3, 4, 5];
+const doubled = numbers.map((num) => num * 2);
+console.log(doubled);
+
+const fruits2 = ["apple", "banana"];
+const uppercase = fruits2.map((fruit) => fruit.toUpperCase());
+console.log(uppercase);
+
+// FILTER - keep only elements that pass a test
+const numbers2 = [1, 2, 3, 4, 5, 6];
+const evens = numbers2.filter((num) => num % 2 === 0);
+console.log(evens);
+
+const words = ["cats", "elephant", "dog", "bird"];
+const longWords = words.filter((word) => word.length > 3);
+console.log(longWords);
+
+// CHAINING methods
+const result = numbers2.filter((num) => num > 2)
+    .map((num) => num * 10).filter((num) => num < 55);
+console.log(result);
