d9495965ec9c8afc250a698b098070f2a9bc5492
MariuswebDev <efuetlanchamarius@gmail.com>
2026-07-17 15:29:05 +0100

    mini project: Interactive Todo List

--- session-07/mini project/todo.js
+++ session-07/mini project/todo.js
@@ -2,75 +2,70 @@ const todoInput = document.getElementById("todoInput");
 const addBtn = document.getElementById("addBtn");
 const todoList = document.getElementById("todoList");
 
-
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
 function renderTodos() {
     todoList.innerHTML = "";
-    const completedCount = todos.filter(todo => todo.completed);
-    const unCompletedCount = todos.filter(todo => !todo.completed);
+    //TRACKING TODOS
+    const completedCount = todos.filter(todo => todo.completed).length;
+    const unCompletedCount = todos.filter(todo => !todo.completed).length;
 
-    document.getElementById("completed").textContent = completedCount.length;
-    document.getElementById("uncompleted").textContent = unCompletedCount.length;
+    document.getElementById("completed").textContent = completedCount;
+    document.getElementById("uncompleted").textContent = unCompletedCount;
     document.getElementById("total").textContent = todos.length;
+
     const empty = document.getElementById("empty");
     if (todos.length === 0) {
-        empty.textContent = "No todos yet!"
+        empty.textContent = "No todos yet!";
     } else {
         empty.textContent = "";
     }
 
-
     todos.forEach(todo => {
-        //CREATING ELEMENTS
         const li = document.createElement("li");
-        li.style.listStyle = "none";
         const span = document.createElement("span");
         const deleteBtn = document.createElement("button");
+        const checkBox = document.createElement("input");
         span.textContent = todo.text;
+
+        //DELETE TODOS
         deleteBtn.textContent = "Delete";
         deleteBtn.style.backgroundColor = "crimson";
         deleteBtn.style.color = "white";
 
-        //CHECKBOX SECTION
-        const checkBox = document.createElement("input");
+        deleteBtn.addEventListener("click", () => {
+            deleteTodos(todo.id)
+        })
+
+        // COMPLETE OR UNCOMPLETED
         checkBox.type = "checkbox";
         checkBox.checked = todo.completed;
         if (todo.completed) {
             span.style.textDecoration = "line-through";
             span.style.color = "gray";
             deleteBtn.style.backgroundColor = "gray";
-
         }
         checkBox.addEventListener("click", () => {
-            toggleCompleted(todo.id);
-        });
-
-        //DELETE SECTION
-        deleteBtn.addEventListener("click", () => {
-            deleteTodos(todo.id)
-        });
-
-
-        //APPEND LIST
-        todoList.appendChild(li)
+            toggleCompleted(todo.id)
+        })
+        todoList.appendChild(li);
         li.appendChild(span);
         li.appendChild(checkBox);
         li.appendChild(deleteBtn);
-    });
+    })
 }
 
 addBtn.addEventListener("click", () => {
     addTodo();
 });
 
-todoInput.addEventListener("keypress", (e) => {
+todoInput.addEventListener("keypress", e => {
     if (e.key === 'Enter') {
-        addTodo();
+        addTodo()
     }
 })
 
-function saveTodos() {
+function savedTodos() {
     localStorage.setItem("todos", JSON.stringify(todos));
 }
 
@@ -83,25 +78,23 @@ function addTodo() {
         completed: false
     }
 
-
     todos.push(newTodo);
     todoInput.value = "";
-
-    saveTodos();
+    savedTodos();
     renderTodos();
 }
 
 function deleteTodos(id) {
     todos = todos.filter(todo => todo.id !== id);
     renderTodos();
+    savedTodos();
 }
 
 function toggleCompleted(id) {
-    todos = todos.map(todo => (
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-    ));
-
+    todos = todos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo);
     renderTodos();
+    savedTodos();
 }
 
 renderTodos();
